f592ddca7a96d0cd8a3b2892b0a5f8414b16dd05
julia-kocik <julia.kocik@gmail.com>
2023-10-01 20:50:40 +0200

    Add changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import OtherPage from "./OtherPage";
-import Fib from "./Fib";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-heder">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
           <a
             className="App-link"
             href="https://reactjs.org"
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -25,11 +25,9 @@ class Fib extends Component {
     });
   }
 
-  testMethod() {
-    console.log('test')
-  }
   handleSubmit = async (event) => {
     event.preventDefault();
+
     await axios.post('/api/values', {
       index: this.state.index,
     });
