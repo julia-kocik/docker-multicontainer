d4dab4eb674824f109cb4ebd0713ad541580a84c
julia-kocik <julia.kocik02@gmail.com>
2023-10-06 18:06:27 +0200

    Test deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Docker multi-container
+              Docker multi container
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
